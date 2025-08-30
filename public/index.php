3f4fd6008025d61573e7c0343f2d6772521c3cb6
hemkdev <lucas_l_hemkemeier@estudante.sesisenai.org.br>
2025-08-30 09:52:35 -0300

    redirecionar

--- public/index.php
+++ public/index.php
@@ -16,6 +16,20 @@
 
            $stmt = $conexao->prepare("SELECT * FROM professor WHERE email_professor = ? AND senha_professor = ? ");
            $stmt -> bind_param("ss", $email, $senha);
+           $stmt -> execute();
+           $resultado = $stmt->get_result();
+
+           if  ($resultado->num_rows === 1) {
+                $dados = $resultado->fetch_assoc();
+
+                $_SESSION['email'] = $dados['email_professor'];
+                $_SESSION['nome'] = $dados['nome_professor'];
+
+                header("location: turma.php");
+                exit;
+            } else {
+                $erro = "Usuário ou senha inválidos";
+            }
         }
     }
 ?>
@@ -39,6 +53,11 @@
         <input type="password" name="senha" required> <br>
 
         <button type="submit" name="login">Entrar</button>
+        <?php
+            if($erro) {
+                echo "<div> $erro </div>";
+            }
+        ?>
     </form>
 </body>
 </html>
